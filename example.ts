eb82857aa49f6076ea1e77979cf763ff90b210b1
Nahve <129984923+Nahve@users.noreply.github.com>
2025-05-06 19:32:41 -0300

    Example1

---
+++ example.ts
@@ -0,0 +1,21 @@
+import promptSync from 'prompt-sync';
+//let nombre: string | null = prompt("Por favor, ingresa tu nombre:");
+
+//if (nombre) {
+  //console.log('Hola ' + nombre);
+//} else {
+//  console.log('No ingresaste ningún nombre.');
+//}
+
+//let nombre: string = "Nahuel"
+
+
+//console.log('Hola'+ nombre)
+
+
+
+const prompt = promptSync();
+
+let nombre: string = prompt("Por favor, ingresa tu nombre:") ?? ""; // El ?? "" proporciona un string vacío si el usuario no ingresa nada
+
+console.log('Hola ' + nombre);
